3c19aa0be376c2d7fb0dec4b7bd8e32fa37f3951
yamasaki <yamasakipann0218@gmail.com>
2022-12-15 04:10:37 +0900

    main 関数

--- Basic/main.go
+++ Basic/main.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/seipan/Go-Authentic/tree/main/Basic/handler"
+)
+
+func main() {
+	historyCreate := http.HandlerFunc(handler.BacicAuth)
+	http.Handle("/api/history/create", historyCreate)
+
+	http.ListenAndServe(":8080", nil)
+}
