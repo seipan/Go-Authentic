8f917b95331fa9e68c19072a9959a5ed41307123
yamasaki <yamasakipann0218@gmail.com>
2022-12-16 01:02:43 +0900

    infra

---
+++ Session/handler/middleware.go
@@ -0,0 +1,29 @@
+package handler
+
+import "net/http"
+
+func checkLogin() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		cookieKey := "_cookie"
+		id := session.GetSession(c, cookieKey)
+		if id == nil {
+			c.Redirect(http.StatusFound, "/login")
+			c.Abort()
+		} else {
+			c.Next()
+		}
+	}
+}
+
+func checkLogout() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		cookieKey := "_cookie"
+		id := session.GetSession(c, cookieKey)
+		if id != nil {
+			c.Redirect(http.StatusFound, "/")
+			c.Abort()
+		} else {
+			c.Next()
+		}
+	}
+}
