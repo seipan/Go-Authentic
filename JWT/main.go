89a9325718a7ae3c964220e501ae843fff019971
yamasaki <yamasakipann0218@gmail.com>
2022-12-18 19:38:06 +0900

    main関数

--- JWT/main.go
+++ JWT/main.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
-	
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type post struct {
+	Title string `json:"title"`
+	Tag   string `json:"tag"`
+	URL   string `json:"url"`
 }
+
+func main() {
+	r := mux.NewRouter()
+	// localhost:8080/publicでpublicハンドラーを実行
+	r.Handle("/public", public)
+
+	//サーバー起動
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
+
+var public = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	post := &post{
+		Title: "VueCLIからVue.js入門①【VueCLIで出てくるファイルを概要図で理解】",
+		Tag:   "Vue.js",
+		URL:   "https://qiita.com/po3rin/items/3968f825f3c86f9c4e21",
+	}
+	json.NewEncoder(w).Encode(post)
+})
